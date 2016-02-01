c0b0d38812944ca8e99e06575ecd9df7417d9266
jsKim <kimjs4384@gmail.com>
2016-02-01 17:42:09 +0900

    add base page

--- gs-addPage/src/main/java/org/geoserver/page/WelcomePage.java
+++ gs-addPage/src/main/java/org/geoserver/page/WelcomePage.java
@@ -1,5 +1,11 @@
 package org.geoserver.page;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.geoserver.web.GeoServerBasePage;
+
 public class WelcomePage extends GeoServerBasePage{
+	public WelcomePage() {
+		add(new Label("welcome","welcome"));
+	}
 	
 }
